5dbaa979d455d686fd843b545c6ddb0a922d3f19
srimanreddysingireddy <srimanreddysingireddy1327@gmail.com>
2024-07-11 15:32:34 +0530

    sending addtion file and about html file

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition
+{
+  public static void main(String args[])
+  {
+    int  n1=20;
+    int n2=20;
+
+    int add=n1+n2;
+    
+    {
+       System.out.println(add);
+    }
+   }
+}
